05d0fa72a342b282c6374c3dca280a1a60f0507f
minaakshii-aanand5 <minakshiaanand89@gmail.com>
2026-03-19 18:44:22 +0530

    Time: 7 ms (81.43%), Space: 19.6 MB (89.93%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s):
+        cleaned = ""
+
+        for ch in s:
+            if ch.isalnum():
+                cleaned += ch.lower()
+
+        return cleaned == cleaned[::-1]
+        
