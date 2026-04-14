5b19c155e10279e83a4d7090a05d222b137b14be
minaakshii-aanand5 <minakshiaanand89@gmail.com>
2026-04-14 22:35:28 +0530

    Time: 0 ms (100%), Space: 19.3 MB (68.54%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -0,0 +1,14 @@
+class Solution:
+    def invertTree(self, root):
+        if not root:
+            return None
+        
+        # swap children
+        root.left, root.right = root.right, root.left
+        
+        # recursive call
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        
+        return root
+        
