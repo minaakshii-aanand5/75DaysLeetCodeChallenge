788539267970de4910ff1cf1f71865519160ef9c
minaakshii-aanand5 <minakshiaanand89@gmail.com>
2026-05-25 22:10:11 +0530

    Time: 11 ms (97.86%), Space: 20.6 MB (96.58%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        
+        freq = Counter(tasks)
+        
+        max_freq = max(freq.values())
+        
+        max_count = list(freq.values()).count(max_freq)
+        
+        return max(len(tasks), (max_freq - 1) * (n + 1) + max_count)
+        
