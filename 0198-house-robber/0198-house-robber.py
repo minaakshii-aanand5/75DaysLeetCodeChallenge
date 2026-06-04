3be764a27f517ef429e287cae1f126e5370a89a9
minaakshii-aanand5 <minakshiaanand89@gmail.com>
2026-06-04 20:18:03 +0530

    Time: 0 ms (100%), Space: 19.1 MB (90.17%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        rob1 = 0
+        rob2 = 0
+
+        for money in nums:
+
+            current = max(money + rob1, rob2)
+
+            rob1 = rob2
+            rob2 = current
+
+        return rob2
+        
